23045b7bd3375db36c466f958770159f0ca3163d
Aszmel <mellipone@gmail.com>
2020-11-01 17:13:58 +0100

    maybe add remove all button, more styling, almost done :)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <h1>Vue 3 Todo App</h1>
+  <h1 class="title">Vue 3 Todo App</h1>
   <form @submit.prevent="addNewTodo">
     <label>New Todo</label>
     <input v-model="newTodo" name="newTodo"/>
     <button>Add New Todo</button>
   </form>
-  <h2>{{newTodo}}</h2>
+  <div class="allDone" @click="allDone"><button>Mark All Done</button></div>
+  <ul class="grid">
+    <li v-for="todo in todos" v-bind:key="todo.id" class="todo">
+      <h3 v-bind:class="{done: todo.done}" @click="toggleDone(todo)">{{ todo.content }}</h3>
+      <button @click="removeItem(todo)" class="remove">Remove item</button>
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -15,19 +21,42 @@ export default {
   setup(){
 
     const newTodo = ref('');
+    const todos = ref([]);
 
     function addNewTodo(){
-      console.log("New Todo Added");
-      console.log("newTodo ref is " + newTodo.value);
+      todos.value.push({
+        id: Date.now(),
+        done: false,
+        content: newTodo.value
+      });
+      newTodo.value = '';
+    }
+
+    function toggleDone(todo){
+      todo.done = !todo.done;
+    }
+
+    function removeItem(todo){
+        todos.value.shift(todo);
+      }
+
+    function allDone(){
+      todos.value.forEach((
+        todo) => todo.done = true
+      )
     }
 
     return {
       addNewTodo,
       newTodo,
+      todos,
+      toggleDone,
+      removeItem,
+      allDone
+
     }
   }
   }
-
 </script>
 
 <style>
@@ -49,7 +78,36 @@ input, textarea, button, p, div, section, article, select {
 .todo {
   cursor: pointer;
 }
+
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: auto;
+}
+
 .done {
   text-decoration: line-through;
 }
+li{
+  list-style-type: none;
+  }
+.done {
+  text-decoration: line-through;
+}
+.remove {
+  width: 100px;
+  display: flex;
+  font-size: 1.5rem;
+  align-content: space-around;
+}
+.allDone{
+  display: grid;
+  justify-content: center;
+  margin-right: 50px;
+}
+.title {
+  color: lightgreen;
+  display: flex;
+  justify-content: center;
+  }
 </style>
